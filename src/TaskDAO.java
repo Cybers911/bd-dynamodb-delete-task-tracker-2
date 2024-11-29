5fda0803ce0b53203f6c78d0acbb7f93d614a61a
Cybers911 <info@cybers911.com>
2024-11-28 18:47:41 -0600

    item deleted within attribute year 2016

--- src/TaskDAO.java
+++ src/TaskDAO.java
@@ -1,5 +1,11 @@
-
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBDeleteExpression;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ConditionalCheckFailedException;
+import com.amazonaws.services.dynamodbv2.model.ExpectedAttributeValue;
+import java.util.Map;
+import java.util.HashMap;
+
 
 public class TaskDAO {
 
@@ -7,6 +13,7 @@ public class TaskDAO {
 
     /**
      * Allows access to and manipulation of Task objects from the data store.
+     *
      * @param mapper Access to DynamoDB
      */
     public TaskDAO(DynamoDBMapper mapper) {
@@ -15,12 +22,24 @@ public class TaskDAO {
 
     /**
      * Sets a condition that an item is only deleted if 'year_completed' = '2016', otherwise the item is not deleted.
+     *
      * @param task Task instance that is passed into the delete() method to delete if it meets the delete condition
      */
     public void deleteTask(Task task) {
         //TODO: Replace this code to add the delete condition described above and in the README.
-        mapper.delete(task);
+
+        try {
+            DynamoDBDeleteExpression deleteExpression = new DynamoDBDeleteExpression();
+            Map<String, ExpectedAttributeValue> expected = new HashMap<>();
+            expected.put("year_completed", new ExpectedAttributeValue(new AttributeValue().withN("2016")));
+            deleteExpression.setExpected(expected);
+            mapper.delete(task, deleteExpression);
+        } catch (ConditionalCheckFailedException e) {
+            e.printStackTrace();
+        }
     }
+
+
 
     /**
      * Uses the load() method to get an item from the TaskTracker table given a partition key value.
